bfcb8f69fe590e0e202fb12c522cfecb0bb834c4
Maxim Volgin <maxvol@users.noreply.github.com>
2020-11-13 19:33:06 +0100

    Update RxSPMTests.swift

--- Tests/RxSPMTests/RxSPMTests.swift
+++ Tests/RxSPMTests/RxSPMTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import Rx
+@testable import RxSPM
 
 final class RxTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Rx().text, "Hello, World!")
+        XCTAssertEqual(RxSPM().text, "Hello, World!")
     }
 
     static var allTests = [
